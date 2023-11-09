68b857b6350df59f83cea8da5e07e84fb0a2f2fb
Fraser Wilkinson <fraserwilk@gmail.com>
2023-11-09 12:38:06 +1100

    footer edits

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -11,23 +11,30 @@ defined( 'ABSPATH' ) || exit;
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-
+	
 
 	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 
 	<div class="entry-content">
 		<div class="row">
-		<div class="col-sm-2">col-sm-2</div>
+		<div class="col-sm-2"></div>
 		<div class="col-sm-8">
 			<div class="row">
-
+				<?php
+					if ( ! is_page_template( 'page-templates/no-title.php' ) ) {
+						the_title(
+							'<header class="entry-header"><h1 class="entry-title">',
+							'</h1></header><!-- .entry-header -->'
+						);
+					}
+				?>
 				<?php
 				the_content();
 				understrap_link_pages();
 				?>
 			</div>
 		</div>
-		<div class="col-sm-2">col-sm-2</div>
+		<div class="col-sm-2"></div>
 		</div>
 
 	</div><!-- .entry-content -->
